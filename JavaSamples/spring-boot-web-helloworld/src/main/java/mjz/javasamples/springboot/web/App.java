ac53d85ac15f8499f1cfd63abb9718b255a6a068
mj-z <6733471@qq.com>
2017-05-07 19:38:09 +0800

    added spring boot web sample

---
+++ JavaSamples/spring-boot-web-helloworld/src/main/java/mjz/javasamples/springboot/web/App.java
@@ -0,0 +1,25 @@
+package mjz.javasamples.springboot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+
+@SpringBootApplication
+@RestController
+public class App 
+{
+	@RequestMapping("/")
+    public String greeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
