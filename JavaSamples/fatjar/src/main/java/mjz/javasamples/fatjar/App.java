8dc5322cf0b3a122b49a08a50755b5e334e17b38
mj-z <6733471@qq.com>
2017-05-10 21:14:41 +0800

    added simple spring boot + fat jar

---
+++ JavaSamples/fatjar/src/main/java/mjz/javasamples/fatjar/App.java
@@ -0,0 +1,17 @@
+package mjz.javasamples.fatjar;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+
+		List<String> strList = Lists.newArrayList("hi", "world!");
+		System.out.println(strList);
+	}
+}
