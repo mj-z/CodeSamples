0f749fa60c53cab67935a73d53dc16f79d05e3b0
mj-z <6733471@qq.com>
2017-07-05 21:21:38 +0800

    slf4j instances test

--- JavaSamples/java8/src/test/java/mjz/javasamples/java8/AppTest.java
+++ JavaSamples/java8/src/test/java/mjz/javasamples/java8/AppTest.java
@@ -1,38 +1,25 @@
 package mjz.javasamples.java8;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest  {
+	
+	private Logger log1 = LoggerFactory.getLogger(getClass());
+	
+	private static Logger log2 = LoggerFactory.getLogger(AppTest.class);
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	public void test() {
+		System.out.println(log1);
+		System.out.println(log2);
+		System.out.println(log2 == log1);
+		
+		Logger log3 = LoggerFactory.getLogger(getClass());
+		System.out.println(log3 == log1);
+	}
 }
